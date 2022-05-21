df1c2fe0baf634f3e163790817f7f260921cd007
RoastBeefer00 <roastbeefer000@gmail.com>
2022-05-21 18:05:11 -0500

    vercel test

--- api/getRecipe.go
+++ api/getRecipe.go
@@ -50,7 +50,6 @@ func GetRandom(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	w.Header().Set("Content-Type", "application/json")
-}
 
 	json.NewEncoder(w).Encode(recipes.Recipes[i])
 }
