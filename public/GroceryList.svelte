0a53ff9c2c3bf9e1a9bd589b07a74b8a4af5634e
Jake Jasmin <roastbeefer000@gmail.com>
2022-09-11 15:20:57 -0600

    move delete and grocery list to backend

--- public/GroceryList.svelte
+++ public/GroceryList.svelte
@@ -12,78 +12,34 @@
   // const toggle = () => (open = !open);
   function toggle() {
     open = !open;
-    splitIngredients();
+    getIngredients();
   }
 
-  $: sortedGroceryList = sortByIngredient(Object.values($groceryList));
-  $: combinedGroceryList = combineIngredients(sortedGroceryList);
+  let groceries = [];
 
-  function sortByIngredient(array) {
-    array.sort((el1, el2) =>
-      el1.ingredient.toLowerCase().localeCompare(el2.ingredient.toLowerCase())
-    );
-    return array;
-  }
-
-  function splitIngredients() {
-    $groceryList = [];
-    var value;
-    var measurement;
-    var rest;
-    var json;
-    $storeFE.forEach(recipe => {
-      recipe.ingredients.forEach(ingredient => {
-      value = ingredient.match("^\\d*[^a-zA-Z \\*]?\\d*");
-      console.log(value);
-      measurement = ingredient.match("tbsps?|tsps?|cups?|cans?|packages?|packets?|ozs?|pounds?");
-      console.log(measurement);
-      
-      if (measurement == null) {
-        rest = ingredient.match("\\*?[a-zA-Z].*");
-        console.log(rest);
-      }
-      else{
-        rest = ingredient.match("(?<=tbsps? |tsps? |cups? |cans? |packages? |packets? |ozs? |pounds? )\\*?[a-zA-Z].*");
-        console.log(rest);
-      }
-
-      json = "{\"quantity\":\"" + value + "\", \"measurement\":\"" + measurement + "\", \"ingredient\":\"" + rest + "\"}";
-      $groceryList[$groceryList.length] = JSON.parse(json);
-      });
-    });
-  }
-
-  function combineIngredients(array) {
-    for (let i = 0; i < array.length - 1; i++) {
-      var j = i + 1;
-      if (array[i].ingredient.includes(array[j].ingredient) || array[j].ingredient.includes(array[i].ingredient)) {
-        if (array[i].measurement.includes(array[j].measurement) || array[j].measurement.includes(array[i].measurement)) {
-          if (array[i].quantity != "") {
-            if (array[i].ingredient.length > array[j].ingredient.length){
-              array[i].ingredient = array[j].ingredient;
-            }
-            if (array[j].measurement.length > array[i].measurement.length){
-              array[i].measurement = array[j].measurement;
-            }
-            array[i].quantity = parseFloat(array[i].quantity) + parseFloat(array[j].quantity);
-          }
-          array.splice(j, 1);
-          i--;
-        }
-      }
-    }
-    return array;
+  async function getIngredients() {
+    let response = await fetch("http://127.0.0.1:8000/ingredients", {
+			method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+			body: JSON.stringify($storeFE),
+		});
+    
+    let ingredients = await response.json();
+    groceries = ingredients;
   }
 
   function printIngredient(ingredient) {
-    if (ingredient.quantity == '' && ingredient.measurement == "null") {
-      return ingredient.ingredient;
+    if (ingredient.quantity == "" && ingredient.measurement == "") {
+      return ingredient.item;
     }
-    if (ingredient.quantity != '' && ingredient.measurement != "null"){
-      return ingredient.quantity + " " + ingredient.measurement + " " + ingredient.ingredient;
+    if (ingredient.quantity != "" && ingredient.measurement != ""){
+      return ingredient.quantity + " " + ingredient.measurement + " " + ingredient.item;
     }
-    if (ingredient.quantity != '' && ingredient.measurement == "null") {
-      return ingredient.quantity + " " + ingredient.ingredient;
+    if (ingredient.quantity != "" && ingredient.measurement == "") {
+      return ingredient.quantity + " " + ingredient.item;
     }
     
   }
@@ -98,7 +54,7 @@
     <div class="modal_body">
       <Container>
         <Row cols={1} style="max-height:calc(100vh - 300px); overflow:scroll">
-          {#each combinedGroceryList as ingredient}
+          {#each groceries as ingredient}
             <div class="checkbox">
               <input id="ingredient" type ="checkbox" >
               <label for="ingredient"> {printIngredient(ingredient)} </label>
--- public/card.svelte
+++ public/card.svelte
@@ -1,5 +1,5 @@
 <script>
-    function removeRecipe(recipe) {
+    async function removeRecipe(recipe) {
         for (let index = 0; index < $storeFE.length; index++) {
             if ($storeFE[index] == recipe) {
                 var l = $undo.length;
@@ -10,9 +10,20 @@
                 }
             } 
         }
-        
-        console.log($undo)
-		$storeFE = $storeFE.filter(r => r !== recipe);
+
+        let response = await fetch("http://127.0.0.1:8000/delete", {
+			method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+			body: JSON.stringify({
+                recipe: recipe,
+                store: $storeFE
+            }),
+		});
+        let recipes = await response.json();
+        $storeFE = recipes;
 	}
 
     async function replaceRecipe(recipe) {
@@ -26,7 +37,6 @@
                 }
                 let response = await fetch("https://r7qi88.deta.dev/random");
 		        let recipes = await response.json();
-                console.log(recipes[0]);
                 $storeFE[index] = recipes[0];
             } 
         }
@@ -41,14 +51,6 @@
     let visible = false;
 
 	import {
-		Card,
-		CardBody,
-		CardHeader,
-		CardSubtitle,
-		CardTitle,
-		Input,
-		ListGroup,
-		ListGroupItem,
 		Container,
 		Row,
 		Col,
